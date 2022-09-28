c1509d7a49dd09d731ae6ca83a90f1ff09d6608b
Kaysheridan <kaysheridan1@hotmail.com>
2022-09-28 18:16:27 +0000

    foodoptions view added to models.py

--- counterco/models.py
+++ counterco/models.py
@@ -5,10 +5,26 @@ from django.contrib.auth.models import User
 
 
 class AppCustomer(models.Model):
-    names = models.CharField(max_length=150, null=True)
-    email = models.CharField(max_length=200, null=True)
-    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
+    names = models.CharField(max_length=150, null=True)
+    email = models.CharField(max_length=150, null=True)
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return str(self.names)
+
+
+class Foodoptions(models.Model):
+    name = models.CharField(max_length=150)
+    food_group = models.ManyToManyField(Category)
+    calories = models.DecimalField(decimal_places=1, max_digits=5, default=0,
+    blank=True)
+    protein = models.DecimalField(decimal_places=1, max_digits=5, default=0)
+    carbs = models.DecimalField(decimal_places=1, max_digits=5, default=0)
+    fats = models.DecimalField(decimal_places=1, max_digits=5, default=0)
+    amount = models.IntegerField(default=1, blank=True, null=True)
+
+    def __str__(self):
+        return str(self.names)
+
+
