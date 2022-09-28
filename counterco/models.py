6e7b3be800f375368e8d5d2d6a2f306d4e1816ec
Kaysheridan <kaysheridan1@hotmail.com>
2022-09-28 20:03:51 +0000

    models.py ammeded to hold all info on app users, food and food categorys

--- counterco/models.py
+++ counterco/models.py
@@ -14,14 +14,27 @@ class AppCustomer(models.Model):
         return str(self.names)
 
 
+class Meals(models.Model):
+    meal_choices = (
+        ('meal 1', 'breakfast')
+        ('meal 2', 'lunch')
+        ('meal 3', 'dinner')
+        ('meal 4', 'snacks')
+    )
+    name = models.CharField(options=meal_choices, max_length=60)
+
+    def __str__(self):
+        return self.name
+
+
 class Foodoptions(models.Model):
     name = models.CharField(max_length=150)
-    food_group = models.ManyToManyField(Category)
-    calories = models.DecimalField(decimal_places=1, max_digits=5, default=0,
+    food_group = models.ManyToManyField(Meals)
+    calories = models.DecimalField(decimal_places=1, max_digits=6, default=0,
     blank=True)
-    protein = models.DecimalField(decimal_places=1, max_digits=5, default=0)
-    carbs = models.DecimalField(decimal_places=1, max_digits=5, default=0)
-    fats = models.DecimalField(decimal_places=1, max_digits=5, default=0)
+    protein = models.DecimalField(decimal_places=1, max_digits=6, default=0)
+    carbs = models.DecimalField(decimal_places=1, max_digits=6, default=0)
+    fats = models.DecimalField(decimal_places=1, max_digits=6, default=0)
     amount = models.IntegerField(default=1, blank=True, null=True)
 
     def __str__(self):
